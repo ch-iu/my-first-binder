cc3ab66d81101727f643a9e3ff30db225e399e78
ch-iu <chius@me.com>
2024-11-29 14:06:53 +0000

    Update install.R

--- install.R
+++ install.R
@@ -1,8 +1,8 @@
 install.packages("readr")
 install.packages(“shiny”)
 install.packages(“xlsx”)
-
+install.packages(“rJava”)
 install.packages(“xlsxjars”)
 install.packages(“reshape2”)
 install.packages(“ggplot2”)
-
+install.packages(“g2sd”)
